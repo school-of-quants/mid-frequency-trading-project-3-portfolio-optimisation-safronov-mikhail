ef36cd83739525f20c6370d247acd6cd40f1eb3d
Safronov Mikhail <mr.safronovma@gmail.com>
2026-05-06 16:44:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import warnings
+import sys
+import os
+from pathlib import Path
+
+current_file = Path(__file__).resolve()
+
+project_path = current_file.parent.parent 
+
+if str(project_path.parent) not in sys.path:
+    sys.path.insert(0, str(project_path.parent))
+
+
 from equity_project.src.get_data import get_data
 from equity_project.src.run_backtest import run_backtest
 from equity_project.src.train import train
